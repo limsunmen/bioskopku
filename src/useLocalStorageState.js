de2e0f40beb7d5844fcd95d5ff9c6dbcde60c434
Yulianto Alexander <sunmenlim@gmail.com>
2025-06-16 19:35:35 +0700

    adding file

---
+++ src/useLocalStorageState.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+export function useLocalStorageState(initalState, key) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initalState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
